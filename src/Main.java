8c25fef1447e5162269a03bef258f7080081e4b9
alptugozates <omeralptugozates@gmail.com>
2024-01-19 22:09:00 +0300

    Proje tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import com.hamburger.DeluxeBurger;
+import com.hamburger.Hamburger;
+import com.hamburger.HealthyBurger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hamburger hamburger = new Hamburger("Basic", "Beef", 3.56, "Wrap");
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        hamburger.itemizeHamburger();
+
+        System.out.println("\n-----------------\n");
+
+        HealthyBurger healthyBurger = new HealthyBurger("Vegan Burger", "Beef", 5.67, "Sandwich");
+        healthyBurger.addHealthyAddition1("Egg", 5.43);
+        healthyBurger.addHealthyAddition2("Lentils", 3.41);
+        healthyBurger.itemizeHamburger();
+
+        System.out.println("\n-----------------\n");
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 50.53);
+        db.itemizeHamburger();
+
     }
 }
